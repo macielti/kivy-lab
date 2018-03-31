d78b7dfb10211acdf96714b07b113bf251802eba
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2018-03-31 10:44:40 -0300

    teste write file

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.storage.jsonstore import JsonStore
+from os.path import join
+import os
+
 
 
 class Tela(BoxLayout):
     def gerar_arqv(self):
-        with open("test.txt", "w") as file:
+        app_folder = os.path.dirname(os.path.abspath(__file__))
+        with open(app_folder + "/test.txt", "w") as file:
             file.write("\nBruno, testando.\n")
+            
+             
+
+            
     
 
 
 class Teste(App):
     def build(self):
+        
         return Tela()
 
 if __name__ == "__main__":
