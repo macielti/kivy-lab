51054fb57e53c0bd8e9923ea6c8f8e396a88a72e
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2018-03-31 18:40:46 -0300

    teste de escrita de dados do use

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.storage.jsonstore import JsonStore
-from os.path import join
-import os
+from random import randint
 
 
 
 class Tela(BoxLayout):
     def gerar_arqv(self):
-        app_folder = os.path.dirname(os.path.abspath(__file__))
-        with open(app_folder + "/test.txt", "w") as file:
-            file.write("\nBruno, testando.\n")
+        number = str(randint(0,800))
+        path = Teste().user_data_dir
+        with open(path + "/test"+number+".txt", "w") as file:
+            for i in range(0, 8000):
+                file.write("\nBruno, testando.\n")
             
              
 
@@ -20,7 +21,7 @@ class Tela(BoxLayout):
 
 class Teste(App):
     def build(self):
-        
+        Teste().user_data_dir
         return Tela()
 
 if __name__ == "__main__":
